145e59f565b2158bbe7e90a27ce4b053614da4da
Sang Nguyen Tan <sos.coconut@gmail.com>
2026-05-08 20:43:12 +0700

    Handle Bird animation logic and clip
    - Create Bird flyBird() method which handles flying logic
    - Create animation for flying

--- assets/Scripts/Bird.ts
+++ assets/Scripts/Bird.ts
@@ -1,4 +1,4 @@
-import { _decorator, CCFloat, Component, Node, Vec3, Animation } from 'cc';
+import { _decorator, CCFloat, Component, Node, Vec3, Animation, tween, easing } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Bird')
@@ -36,6 +36,22 @@ export class Bird extends Component {
         this.node.setPosition(new Vec3(0, 0, 0));
 
     }
+
+    flyBird() {
+        this.birdAnimation.stop();
+
+        tween(this.node.position)
+            .to(this.jumpDuration, new Vec3(this.node.position.x, this.node.position.y + this.jumpHeight, 0),
+                { easing: 'smooth', onUpdate: (target: Vec3, ratio: number) => {
+                    this.node.position = target;
+                }
+            })
+            .start();
+
+        this.birdAnimation.play();
+
+    }
+
 }
 
 
